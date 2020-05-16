9c4ebc6dfb6f04a346474a2cb778c9634bc56092
24kcs <younghongbo@126.com>
2020-05-16 16:53:32 +0800

    26codes

---
+++ 26-code/05-demo/pages/index/index.js
@@ -0,0 +1,76 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  // 跳转到list页面
+  goList(){
+    // 路由跳转的方法,是可以返回的
+    // wx.redirectTo({
+    //   url:'/pages/list/list'
+    // })
+    wx.navigateTo({
+      url: '/pages/list/list'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
